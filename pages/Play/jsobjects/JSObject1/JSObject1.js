dcfa6f9a0981d6f1b6317ab6c433f2442dcc5989
Bertrand Ndobegang Atemkeng <atemndobs@yahoo.com>
2023-04-24 17:38:14 +0000

    adjust search

--- pages/Play/jsobjects/JSObject1/JSObject1.js
+++ pages/Play/jsobjects/JSObject1/JSObject1.js
@@ -5,9 +5,12 @@ export default {
 	},
 	selectd_song_play (currentSong = {}) {
 		
+		console.log(currentSong)
 		search_results_player.show = true
+		search_results_player.isVisible = true
+		song_search_results.isVisible = false
 		player_song.autoPlay
-		return currentSong.path ?? ""
+		return currentSong
 	},
 	async get_related_songs (currentSong = {}) {
 		let related_url = currentSong.related_songs ?? "";
