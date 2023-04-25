664d35f8e150c6a2ded7f238bcfea8acb3c00c53
Bertrand Ndobegang Atemkeng <atemndobs@yahoo.com>
2023-04-25 15:42:11 +0000

    add related songs

--- pages/Play/jsobjects/play_page_functions/play_page_functions.js
+++ pages/Play/jsobjects/play_page_functions/play_page_functions.js
@@ -1,21 +1,40 @@
 export default {
-	search_results_player: {
+	play_page_init_data : {
 		'show' : false,
 		'currentSong' : {},
 		'search_limit' : 3,
 		'search_hits' : 0,
 		'match_limit' : 3,
-		'related_songs' : "dj_zinhle_indlovu_feat_loyisomp3"
+		'related_songs' : 'my_waymp3'
 	},
 	
 	selectd_song_play (currentSong = {}) {
+
 		search_results_player.isVisible = true;
 		song_search_results.isVisible = false;
+					
+		if(Object.keys(currentSong).length === 0 || currentSong.slug !== "undefined" ){
+				appsmith.store.related_songs = currentSong.slug
+		}
+		get_song_matches.run();
 		player_song.autoPlay;
-		search_results_player.related_songs = currentSong.related_songs
-		let song_matches = get_song_matches.run()
 		
-		console.log(get_song_matches.data)
+		console.log({
+			'currentSong_before_match_req' : currentSong,
+			'related_song_from_current' : currentSong.slug,
+			'related_song_url' : appsmith.store.related_songs
+		})
+	
+		
+		setTimeout(
+			() => {
+				console.log({'related_song_from_store' : appsmith.store.related_songs});
+				//get_song_matches.run();
+				console.log({'response_': get_song_matches.data()})
+			},
+			360
+		)
+
 		
 		return currentSong
 	},
@@ -40,17 +59,23 @@ export default {
 		if(search.text === "") {
 			return;
 		}
+		
+		console.log(appsmith.store)
 		clear_search.isVisible = true
 		song_search_api.run()
 		song_search_results.isVisible = true
 	},
 	
-	async clear_search(){
+	clear_search(){
+		search.text = "";
+		search.inputText = "";
+		search.inputText.trim();
+		search.text.trim();
 		song_search_results.isVisible = false;
 		search_results_player.isVisible = false;
 		song_search_api.clear();
 		clear_search.isVisible = false
-		search.text = "";
-	
+		appsmith.store.related_songs = this.play_page_init_data.related_songs
+		// appsmith.store.related_songs = ""
 	}
 }
