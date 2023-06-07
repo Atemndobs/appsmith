e7694f9dcd91fd02e9e9508d9f137ce2d7455f3f
Bertrand Ndobegang Atemkeng <atemndobs@yahoo.com>
2023-06-07 10:05:34 +0000

    add key

---
+++ pages/Settings/jsobjects/download_song/download_song.js
@@ -0,0 +1,32 @@
+export default {
+	
+		async get_downloaded_song() {
+			console.log(appsmith.store)
+	  //return this.selectd_song_play()
+	},
+			async download(dl_source) {
+				let response = null
+				
+				if(dl_source == 'spotify') {
+					appsmith.store.spotify_url = link.inputText
+					download_spotify.run()
+					response =  download_spotify.data
+					get_result_button.isVisible = true
+					get_result_button.text  = download_spotify.data.data[3]
+				}
+				if(dl_source == 'sc') {
+					appsmith.store.sc_url = link.inputText
+					download_sc.run()
+					if(download_sc.data){
+						get_result_button.isVisible = true
+						get_result_button.text = download_sc.data.data[3]
+						download_result_link.isVisible = true
+					}
+					response =  download_sc.data
+					source.isVisible = true
+				}
+			console.log({'Sore Values' : appsmith.store})
+				
+	  return response
+	}
+}
