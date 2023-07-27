60fcb86d965f82dfc7c3977fdcba632d61a506f9
Bertrand Ndobegang Atemkeng <atemndobs@yahoo.com>
2023-07-27 12:10:45 +0000

    adjust store values

--- pages/settings/jsobjects/settings_page/settings_page.js
+++ pages/settings/jsobjects/settings_page/settings_page.js
@@ -1,6 +1,13 @@
-{
-  "slug" : {{appsmith.store.related_songs}},
-  "bpm": {{appsmith.store.bpm.value = 'undefined'? 100 : appsmith.store.bpm.value }},
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async set_criteria (matchCriteria) {
+		
+
   "bpmRange": {{appsmith.store.set_bpm_range}},
   "happy": {{appsmith.store.happy}},
   "sad": {{appsmith.store.sad}},
@@ -11,5 +18,8 @@
 	"genre":{{appsmith.store.genre}},
   "danceability" : 90,
 	"aggressiveness" : 50,
-	
+		// appsmith.store.related_songs
+		appsmith.store.bpm.value = matchCriteria.bpm
+		return set_match_criteria.run()
+	}
 }
